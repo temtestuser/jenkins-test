e19ad9f3492ecf4543d67680d715fd3530c7b205
temtestuser <44535823+temtestuser@users.noreply.github.com>
2020-11-19 16:01:26 -0800

    Create Pipeline.groovy

---
+++ Pipeline.groovy
@@ -0,0 +1,21 @@
+class Pipeline {
+    def script
+    def configurationFile
+
+    Pipeline(script, configurationFile) {
+        this.script = script
+        this.configurationFile = configurationFile
+    }
+
+    def execute() {
+//    ===================== Your Code Starts Here =====================
+//    Note : use "script" to access objects from jenkins pipeline run (WorkflowScript passed from Jenkinsfile)
+//           for example: script.node(), script.stage() etc
+
+//    ===================== Parse configuration file ==================
+
+//    ===================== Run pipeline stages =======================
+
+//    ===================== End pipeline ==============================
+    }
+}
